13a060d5acbca74bbfd862dcbd2660f786fb8089
ElissonDeSous <elissondsousa@gmail.com>
2026-03-07 11:49:29 -0300

    alterando retorno da imgController pages

--- src/controllers/pages/imgController.js
+++ src/controllers/pages/imgController.js
@@ -59,8 +59,11 @@ export default class imagePages{
                 })
 
                 imagens.push(imagemPages)
-                 return res.status(200).json(imagens)
+                return res.status(201).json({imagemPages})
                }
+            
+
+               
             
          
 
